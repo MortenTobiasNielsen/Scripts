2eb6ef3c31d8b232cd7aafecdb0d8ac99224539d
Morten <mtnielsen89@gmail.com>
2024-06-22 18:33:15 +0200

    Fixed typo

--- GetWildcardCertificate.sh
+++ GetWildcardCertificate.sh
@@ -1,9 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y certbot python3-certbot-dns-cloudflare zip
 
-# setup variables and ask for input from the user
-home_directory=$(eval echo ~${USER})
-
 echo 
 read -p "Enter email for the cloudflare account which owns the domain: " email
 read -p "Enter cloudflare api key: " apiKey
@@ -29,14 +26,14 @@ dns_cloudflare_api_key = $apiKey
 sudo chmod 0400 ~/.secrets/cloudflare.ini
 
 # Create certificate
-sudo certbot certonly --dns-cloudfare --dns-cloudflare-cerdentials $home_directory/.secrets/cloudflare.ini -d $domain,*.$domain --preferred-challanges dns-01
+sudo certbot certonly --dns-cloudfare --dns-cloudflare-credentials ~/.secrets/cloudflare.ini -d $domain,*.$domain --preferred-challanges dns-01
 
 # Zip up the files
 sudo zip -r lib_letsencrypt.zip /var/lib/letsencrypt/
 sudo zip -r etc_letsencrypt.zip /etc/letsencrypt/
 
 # Move the files to the provided path
-mv -f lib_letsencrypt.zip $home_directory/$path
-mv -f etc_letsencrypt.zip $home_directory/$path
+mv -f lib_letsencrypt.zip ~/$path
+mv -f etc_letsencrypt.zip ~/$path
 
-printf "\nA certificate has been made, the files have been zipped up, and moved to $home_directory/$path"
+printf "\nA certificate has been made, the files have been zipped up, and moved to ~/$path"
