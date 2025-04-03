d6329798f5dfd9206147d8b1ad2c8a21e5325c9d
MortenTobiasNielsen <mtnielsen89@gmail.com>
2025-04-03 09:39:59 +0200

    updated cursor install

--- setup-dev.sh
+++ setup-dev.sh
@@ -16,31 +16,22 @@ sudo apt install -y nodejs
 installCursor() {
     if ! [ -f /opt/cursor.appimage ]; then
         echo "Installing Cursor AI IDE..."
-
-        # URLs for Cursor AppImage and Icon
-        CURSOR_URL="https://downloader.cursor.sh/linux/appImage/x64"
-        ICON_URL="https://raw.githubusercontent.com/rahuljangirwork/copmany-logos/refs/heads/main/cursor.png"
-
-        # Paths for installation
         APPIMAGE_PATH="/opt/cursor.appimage"
         ICON_PATH="/opt/cursor.png"
         DESKTOP_ENTRY_PATH="/usr/share/applications/cursor.desktop"
 
-        # Download Cursor AppImage
-        echo "Downloading Cursor AppImage..."
-        sudo curl -L $CURSOR_URL -o $APPIMAGE_PATH
+        echo "Copy Cursor AppImage..."
+        sudo cp Cursor.AppImage $APPIMAGE_PATH
         sudo chmod +x $APPIMAGE_PATH
 
-        # Download Cursor icon
         echo "Downloading Cursor icon..."
-        sudo curl -L $ICON_URL -o $ICON_PATH
+        sudo cp icon.png $ICON_PATH
 
-        # Create a .desktop entry for Cursor using tee.
         echo "Creating .desktop entry for Cursor..."
-        sudo tee $DESKTOP_ENTRY_PATH > /dev/null <<EOL
+        sudo bash -c "cat > $DESKTOP_ENTRY_PATH" <<EOL
 [Desktop Entry]
-Name=Cursor AI IDE
-Exec=$APPIMAGE_PATH
+Name=Cursor AI
+Exec=$APPIMAGE_PATH --no-sandbox
 Icon=$ICON_PATH
 Type=Application
 Categories=Development;
